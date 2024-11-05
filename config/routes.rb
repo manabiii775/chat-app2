d9d457885772d2869abb37e32288996b26dfba48
manabiii775 <ke218.nnkdal@icloud.com>
2024-11-05 20:41:51 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"]
   devise_for :users
-  root to: "messages#index"
+  root to: "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
 end
